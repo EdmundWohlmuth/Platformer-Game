33c1d3d3501dee5d3ef78761b4074adffd186ce1
Edmund Wohlmuth <edmundwoh@gmail.com>
2021-10-19 17:19:32 -0300

    0.3.6 - Minor Update

--- Assets/UserInterface/MainMenu.cs
+++ Assets/UserInterface/MainMenu.cs
@@ -4,8 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator animator;
+    private int levelToLoad;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        FadeToLevel(1);       
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
     }
 }
